bd1787cf9e2f7096a699d4a9dc1dac181771e77d
上官镜文 <shangguanjingwen@zj.tech>
2022-09-03 23:18:28 +0800

    添加客户端负载均衡算法

--- src/main/java/com/bruce/coupon/CouponBackendApplication.java
+++ src/main/java/com/bruce/coupon/CouponBackendApplication.java
@@ -1,11 +1,19 @@
 package com.bruce.coupon;
 
+import com.bruce.coupon.loadbalance.CanaryRuleConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @ComponentScan(basePackages = {"com.bruce"})
+@EnableDiscoveryClient
+@LoadBalancerClient(value = "coupon-template", configuration = CanaryRuleConfiguration.class)
 public class CouponBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CouponBackendApplication.class, args);
---
+++ src/main/java/com/bruce/coupon/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.bruce.coupon.constant;
+
+public interface Constant {
+
+    final static String TRAFFIC_VERSION = "traffic-version";
+
+}
