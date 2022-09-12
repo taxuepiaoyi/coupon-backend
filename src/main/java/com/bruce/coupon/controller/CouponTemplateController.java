5a6828e96729075f353fd2bce8dceabfd47e9563
上官镜文 <shangguanjingwen@zj.tech>
2022-09-12 10:58:44 +0800

    构建sentinel降级处理

--- src/main/java/com/bruce/coupon/controller/CouponTemplateController.java
+++ src/main/java/com/bruce/coupon/controller/CouponTemplateController.java
@@ -7,6 +7,7 @@ import com.bruce.coupon.domain.CouponTemplateHttpInfo;
 import com.bruce.coupon.domain.PagedCouponTemplateHttpInfo;
 import com.bruce.coupon.domain.query.TemplateHttpSearchParams;
 import com.bruce.coupon.service.CouponTemplateHttpService;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -59,7 +60,7 @@ public class CouponTemplateController {
 
     // 批量获取
     @GetMapping("/getTemplateInBatch")
-    @SentinelResource(value = "getTemplateInBatch" ,blockHandler = "getTemplateInBatchBlockHandler")
+    @SentinelResource(value = "getTemplateInBatch" ,blockHandler = "getTemplateInBatchBlockHandler",fallback = "getTemplateInBatchFallBack")
     public Map<Long, CouponTemplateHttpInfo> getTemplateInBatch(@RequestParam("ids") Collection<Long> ids) {
         log.info("getTemplateInBatch: {}", JSON.toJSONString(ids));
         return couponTemplateHttpService.getTemplateInfoMap(ids);
@@ -68,7 +69,13 @@ public class CouponTemplateController {
     //批量获取优惠券的降级处理
     private Map<Long,CouponTemplateHttpInfo> getTemplateInBatchBlockHandler(Collection<Long> ids, BlockException exception){
         log.info("getTemplateInBatchBlockHandler限流了");
-        return Map.of() ;
+        return Maps.newHashMap() ;
+    }
+
+    //批量获取优惠券的降级处理
+    private Map<Long,CouponTemplateHttpInfo> getTemplateInBatchFallBack(Collection<Long> ids, BlockException exception){
+        log.info("getTemplateInBatchFallBack....降级处理");
+        return Maps.newHashMap() ;
     }
 
     // 搜索模板
